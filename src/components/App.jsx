6f5cd712857b6b0c3b7c0510ab2904941ae6055b
OlenaIa <121794911+OlenaIa@users.noreply.github.com>
2023-07-17 20:13:47 +0300

    task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import css from "./App.module.css"
+import { Feedback } from "./Feedback/Feedback";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.main}>
+      <h1>React homework template</h1>
+      <Feedback />
     </div>
   );
 };
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,11 @@
+export const FeedbackOptions = ({options, onLeaveFeedback}) => {
+    return (<>
+        {options.map(option => (<button
+            type='button'
+            key={option}
+            onClick={() => onLeaveFeedback(option)}>
+            {option.toUpperCase()}</button>
+        ))}
+    </>
+    );
+};
